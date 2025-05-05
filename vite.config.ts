d4402a4ab3554a6ab540d5495e9fa12ef6d3b546
Himanshu Rawat <113584120+himanshu-rawat77@users.noreply.github.com>
2025-05-06 01:08:40 +0530

    Added Complete Project

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+    },
+  },
+  define: {
+    'global': 'globalThis',
+    'process.env': {}
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  }
+}));
